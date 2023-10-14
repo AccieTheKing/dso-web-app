42370697bfe350dcf2664c0374e4ce085479f045
Acdaling Edusei <acdaling.edusei@oberon.nl>
2023-10-15 01:20:57 +0200

    Added Tailwind, shadcn and prettier

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+});
+
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" type="image/png" href="/favicon.ico" />
+        <meta
+          name="theme-color"
+          content="#18181b"
+          media="(prefers-color-scheme: light)"
+        />
+        <meta name="theme-color" content="#f4f4f5" />
+        <link rel="apple-touch-icon" href="/icon-maskable-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body>
         <Main />
         <NextScript />
